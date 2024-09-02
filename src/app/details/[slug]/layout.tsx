e1cdc2a553a99e0291feabcf3016c514a4fe8875
aldhinoerman <aldynoerman@gmail.com>
2024-09-02 23:20:33 +0800

    fix hamburger

--- src/app/details/[slug]/layout.tsx
+++ src/app/details/[slug]/layout.tsx
@@ -1,9 +1,10 @@
-import { Footer } from "@/components";
+import { Footer, StickyHeader } from "@/components";
 import React, { PropsWithChildren } from "react";
 
 const DetailLayout: React.FC<PropsWithChildren> = ({ children }) => {
   return (
     <>
+      <StickyHeader />
       <main className="max-w-[1440px] mx-auto px-4 md:px-12">{children}</main>
       <Footer />
     </>
